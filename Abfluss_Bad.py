02d37d23c46fa2da39e71402c41e66fc07138d30
Markus Röleke <m.roeleke@googlemail.com>
2024-11-10 13:43:38 +0100

    add bad abfluss

--- Abfluss_Bad.py
+++ Abfluss_Bad.py
@@ -10,15 +10,33 @@ from freecad_lib import SolidText
 new_doc('Dusche')
 
 # Parameter all in mm
-einlass_durchmesser = 245
-einlass_höhe = 200
+sieb_durchmesser = 85.5
+sieb_höhe = 1
 
-siphon = Cylinder("Siphon", einlass_durchmesser, 20)
-
-show(siphon, name=siphon.name)
+sieb = Cylinder("sieb", sieb_durchmesser, sieb_höhe)
 
 
-export('Dusche', "Siphon")
+loch_durchmesser = 4
+for x_offset in range(-84, 84, loch_durchmesser*2):
+    for y_offset in range(-84, 84, loch_durchmesser*2):
+        
+        distance = math.sqrt(x_offset**2 + y_offset**2)
+        
+        # check ob loch innerhalb kreis liegt
+        if distance < (sieb_durchmesser/2-loch_durchmesser):
+
+            loch = Cylinder("loch", loch_durchmesser, sieb_höhe, Vector(x_offset, y_offset))
+            #print(x_offset, y_offset, distance)
+            #show(loch, name=loch.name)
+            sieb.solid = sieb.solid.cut(loch.solid)
+
+handle_durchmesser = 16
+handle = Cylinder("handle", handle_durchmesser, sieb_höhe+5)
+sieb.solid = sieb.solid.fuse(handle.solid)
+show(sieb, name=sieb.name)
+
+
+export('Dusche', "sieb")
 # show part
 FreeCAD.Gui.activeDocument().activeView().viewIsometric()
 FreeCAD.Gui.SendMsgToActiveView("ViewFit")
