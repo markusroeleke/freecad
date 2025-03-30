edd14addca4bf3874120e3dfe2fd06c412500993
Markus Röleke <m.roeleke@googlemail.com>
2025-03-30 23:05:53 +0200

    update flugbrettchen

--- projekte/Flugbrettchen.py
+++ projekte/Flugbrettchen.py
@@ -9,17 +9,38 @@ from freecad_lib import SolidText
 # create new document
 new_doc('Beute')
 
+# (1) ength  : (w)est   (X-)  (s)outh  (Y-)  (d)own   (Z-)
+# (b)readth  : (c)entre (X0)  (m)iddle (YO)  (g)round (Z0)
+# (h) eight  : (e)ast   (X+)  (n) orth (Y+)  (u)p     (Z+)
+
 # Parameter all in mm
-außen_breite = 150
-außen_höhe = 10
+außen_breite = 160
+außen_höhe = 15
 außen_tiefe = 80
 
-flugbrettchen = Box(Vector(-außen_tiefe/2, -außen_breite/2, 0),
-                    Vector( außen_tiefe/2,  außen_breite/2, außen_höhe))
+flugbrettchen = Box(Vector(-außen_breite/2, -außen_tiefe/2, 0),
+                    Vector( außen_breite/2,  außen_tiefe/2, außen_höhe))
+flugbrettchen.solid = flugbrettchen.solid.makeChamfer(außen_höhe-2, außen_tiefe-5, [flugbrettchen.solid.Edge4])
+
+# unterlegscheibe
+u_durchmesser = 12
+u_dicke = 2
+u_distanz = 120
+offset1 = Vector(flugbrettchen.c-u_distanz/2, flugbrettchen.n-u_dicke, flugbrettchen.d)
+
+u_scheibe_slide1 = Box(Vector(-u_durchmesser/2, -u_dicke/2, 0) + offset1,
+                       Vector( u_durchmesser/2,  u_dicke/2, u_durchmesser+1) + offset1) 
+#show(u_scheibe_slide1, name="u_scheibe_slide1")
+flugbrettchen.solid = flugbrettchen.solid.cut(u_scheibe_slide1.solid)
+
+offset2 = Vector(flugbrettchen.c+u_distanz/2, flugbrettchen.n-u_dicke, flugbrettchen.d)
+
+u_scheibe_slide2 = Box(Vector(-u_durchmesser/2, -u_dicke/2, 0) + offset2,
+                       Vector( u_durchmesser/2,  u_dicke/2, u_durchmesser+1) + offset2) 
+#show(u_scheibe_slide2, name="u_scheibe_slide2")
+flugbrettchen.solid = flugbrettchen.solid.cut(u_scheibe_slide2.solid)
 
 show(flugbrettchen, name="flugbrettchen")
-
-
 export('Beute', "flugbrettchen")
 
 # show part
